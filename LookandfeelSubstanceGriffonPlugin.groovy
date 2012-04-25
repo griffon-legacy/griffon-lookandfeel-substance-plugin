4b6a97ff1c98e38c0aadaf9fac5912f8fbe70b1b
aalmiray <aalmiray@gmail.com>
2012-04-25 13:47:14 +0200

    Updates for Griffon 0.9.5

--- LookandfeelSubstanceGriffonPlugin.groovy
+++ LookandfeelSubstanceGriffonPlugin.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 the original author or authors.
+ * Copyright 2010-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License.
@@ -18,19 +18,76 @@
  * @author Andres Almiray
  */
 class LookandfeelSubstanceGriffonPlugin {
-    def version = 0.4
-    def griffonVersion = '0.9.3 > *'
-    def dependsOn = [lookandfeel: 0.5]
-    def toolkits = ['swing']
-    def license = 'Apache Software License 2.0'
+    // the plugin version
+    String version = '0.5'
+    // the version or versions of Griffon the plugin is designed for
+    String griffonVersion = '0.9.5 > *'
+    // the other plugins this plugin depends on
+    Map dependsOn = [lookandfeel: '0.6']
+    // resources that are included in plugin packaging
+    List pluginIncludes = []
+    // the plugin license
+    String license = 'Apache Software License 2.0'
+    // Toolkit compatibility. No value means compatible with all
+    // Valid values are: swing, javafx, swt, pivot, gtk
+    List toolkits = ['swing']
+    // Platform compatibility. No value means compatible with all
+    // Valid values are:
+    // linux, linux64, windows, windows64, macosx, macosx64, solaris
+    List platforms = []
+    // URL where documentation can be found
+    String documentation = ''
+    // URL where source can be found
+    String source = 'https://github.com/griffon/griffon-lookandfeel-substance-plugin'
 
-    def author = 'Andres Almiray'
-    def authorEmail = 'aalmiray@users.sourceforge.net'
-    def title = 'Substance Swing Look & Feel'
-    def description = '''
-Substance Swing Look & Feel.
+    List authors = [
+        [
+            name: 'Andres Almiray',
+            email: 'aalmiray@yahoo.com'
+        ]
+    ]
+    String title = 'Pluggable Look & Feel based on Substance'
+    String description = '''
+Pluggable Look & Feel based on [Substance][1].
+
+Usage
+-----
+
+Refer to the [lookandfeel][2] plugin to find out how setup `lookAndFeel` and `theme` properties in the configuration.
+
+LookAndFeel and Themes
+----------------------
+| *LookAndFeel* | *Theme*            |
+| ------------- | ------------------ |
+| Substance     | Autum              |
+|               | BusinessBlackSteel |
+|               | BusinessBlueSteel  |
+|               | Business           |
+|               | ChallengerDeep     |
+|               | CremeCoffee        |
+|               | Creme              |
+|               | DustCoffee         |
+|               | DustLook           |
+|               | EmeraldDusk        |
+|               | Gemini             |
+|               | GraphiteAqua       |
+|               | GraphiteGlass      |
+|               | Graphite           |
+|               | Magellan           |
+|               | Mariner            |
+|               | MistAqua           |
+|               | MistSilver         |
+|               | Moderate           |
+|               | NebulaBrickWall    |
+|               | Nebula             |
+|               | OfficeBlue2007     |
+|               | OfficeBlack2007    |
+|               | OfficeSilver2007   |
+|               | Raven              |
+|               | Sahara             |
+|               | Twilight           |
+
+[1]: http://java.net/projects/substance
+[2]: /plugin/lookandfeel
 '''
-
-    // URL to the plugin's documentation
-    def documentation = 'http://griffon.codehaus.org/Lookandfeel+Substance+Plugin'
 }
--- griffon-app/conf/BuildConfig.groovy
+++ griffon-app/conf/BuildConfig.groovy
@@ -2,14 +2,19 @@ griffon.project.dependency.resolution = {
     inherits("global")
     log "warn"
     repositories {
-        griffonPlugins()
         griffonHome()
-        griffonCentral()
         mavenCentral()
     }
     dependencies {
-        compile('com.github.insubstantial:substance:6.2') { transitive = false }
-        compile('com.github.insubstantial:trident:6.2') { transitive = false }
+        String substanceVersion = '7.1'
+        compile("com.github.insubstantial:substance:$substanceVersion",
+                "com.github.insubstantial:laf-widget:$substanceVersion",
+                "com.github.insubstantial:laf-plugin:$substanceVersion",
+                "com.github.insubstantial:substance-swingx:$substanceVersion",
+                "com.github.insubstantial:substance-flamingo:$substanceVersion",
+                "com.github.insubstantial:trident:$substanceVersion") {
+            transitive = false
+        }
     }
 }
 
@@ -21,4 +26,16 @@ griffon {
     }
 }
 
-griffon.jars.destDir='target/addon'
+log4j = {
+    // Example of changing the log pattern for the default console
+    // appender:
+    appenders {
+        console name: 'stdout', layout: pattern(conversionPattern: '%d [%t] %-5p %c - %m%n')
+    }
+
+    error 'org.codehaus.griffon',
+          'org.springframework',
+          'org.apache.karaf',
+          'groovyx.net'
+    warn  'griffon'
+}
